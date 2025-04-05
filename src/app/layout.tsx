7fd01c3d2fc440fa66dce272f9c2569f2fbb85fb
trafficlunar <hello@trafficlunar.net>
2025-04-05 20:34:11 +0100

    feat: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Lexend } from "next/font/google";
 
 import "./globals.css";
@@ -20,9 +21,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${lexend.className} antialiased flex flex-col items-center`}>
-				<Header />
+			<body className={`${lexend.className} antialiased flex flex-col items-center min-h-screen`}>
+				<Script defer src="https://analytics.trafficlunar.net/script.js" data-website-id="bc530384-9b7d-471a-b2e3-f9859da50c24" />
 
+				<Header />
 				<div className="px-4 py-8 max-w-7xl w-full">{children}</div>
 			</body>
 		</html>
