101edfbc16b8fc6120e857c4be9e8ee7dd2c9c20
trafficlunar <hello@trafficlunar.net>
2025-04-25 16:00:04 +0100

    feat: logo and favicon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { Icon } from "@iconify/react";
 
@@ -11,16 +12,17 @@ export default async function Header() {
 	const session = await auth();
 
 	return (
-		<header className="sticky top-0 z-50 w-full p-4 grid grid-cols-3 gap-2 gap-x-4 items-center bg-amber-50 border-b-4 border-amber-500 shadow-md max-lg:grid-cols-2 max-sm:grid-cols-1">
-			<Link href={"/"} className="font-black text-3xl tracking-wide text-orange-400 max-sm:text-center max-sm:col-span-3">
+		<header className="sticky top-0 z-50 w-full p-4 grid grid-cols-3 gap-2 gap-x-4 items-center bg-amber-50 border-b-4 border-amber-500 shadow-md max-lg:grid-cols-2 max-md:grid-cols-1">
+			<Link href={"/"} className="font-black text-3xl tracking-wide text-orange-400 flex items-center gap-2 max-md:justify-center max-md:col-span-2">
+				<Image src="/logo.svg" width={56} height={56} alt="logo" />
 				TomodachiShare
 			</Link>
 
-			<div className="flex justify-center max-lg:justify-end max-sm:col-span-3 max-sm:justify-center">
+			<div className="flex justify-center max-lg:justify-end max-md:justify-center">
 				<SearchBar />
 			</div>
 
-			<ul className="flex justify-end gap-3 items-center h-11 *:h-full max-lg:col-span-2 max-sm:justify-center max-sm:col-span-3">
+			<ul className="flex justify-end gap-3 items-center h-11 *:h-full max-lg:col-span-2 max-md:justify-center">
 				<li title="Random Mii">
 					<Link href={"/random"} className="pill button !p-0 h-full aspect-square">
 						<Icon icon="mdi:dice-3" fontSize={28} />
