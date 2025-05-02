6ce3daadf56ac42ebd7b1b03c1a9af47df0096c9
trafficlunar <hello@trafficlunar.net>
2025-05-02 22:05:36 +0100

    style: remove tracking from header title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,7 +13,7 @@ export default async function Header() {
 
 	return (
 		<header className="sticky top-0 z-50 w-full p-4 grid grid-cols-3 gap-2 gap-x-4 items-center bg-amber-50 border-b-4 border-amber-500 shadow-md max-lg:grid-cols-2 max-md:grid-cols-1">
-			<Link href={"/"} className="font-black text-3xl tracking-wide text-orange-400 flex items-center gap-2 max-md:justify-center max-md:col-span-2">
+			<Link href={"/"} className="font-black text-3xl text-orange-400 flex items-center gap-2 max-md:justify-center max-md:col-span-2">
 				<Image src="/logo.svg" width={56} height={56} alt="logo" />
 				TomodachiShare
 			</Link>
