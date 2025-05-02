e3382183efc39e5df6348ca5b6eca56c957dd4a7
trafficlunar <hello@trafficlunar.net>
2025-05-02 22:17:42 +0100

    fix: update robots.txt to include /report and /admin

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
 		rules: {
 			userAgent: "*",
 			allow: "/",
-			disallow: ["/create-username", "/edit/*", "/profile/settings", "/random", "/submit"],
+			disallow: ["/create-username", "/edit/*", "/profile/settings", "/random", "/submit", "/report/mii/*", "/report/user/*", "/admin"],
 		},
 		sitemap: `${process.env.BASE_URL}/sitemap.xml`,
 	};
