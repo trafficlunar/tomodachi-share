ba5c5e40fd317c9fe6d6fae5ed9bd023dc698515
trafficlunar <hello@trafficlunar.net>
2025-05-04 19:49:02 +0100

    fix: banner text on small screens

--- src/components/admin/banner.tsx
+++ src/components/admin/banner.tsx
@@ -14,8 +14,8 @@ export default function AdminBanner() {
 	if (!data || !data.message) return null;
 
 	return (
-		<div className="w-full h-10 bg-orange-300 border-y-2 border-y-orange-400 mt-1 shadow-md flex justify-center items-center gap-2 text-orange-900 font-semibold">
-			<Icon icon="humbleicons:exclamation" className="text-2xl" />
+		<div className="w-full h-10 bg-orange-300 border-y-2 border-y-orange-400 mt-1 shadow-md flex justify-center items-center gap-2 text-orange-900 text-nowrap overflow-x-auto font-semibold max-sm:justify-start">
+			<Icon icon="humbleicons:exclamation" className="text-2xl min-w-6" />
 			<span>{data.message}</span>
 		</div>
 	);
