33ec0f059b6f349a6f15a3b991441ddc7b2e1816
trafficlunar <hello@trafficlunar.net>
2025-05-24 16:28:36 +0100

    feat: make messages and buttons more island themed
    silly :3

--- src/app/create-username/page.tsx
+++ src/app/create-username/page.tsx
@@ -23,7 +23,7 @@ export default async function CreateUsernamePage() {
 	return (
 		<div className="flex-grow flex items-center justify-center">
 			<div className="bg-amber-50 border-2 border-amber-500 rounded-2xl shadow-lg px-10 py-12 max-w-md text-center">
-				<h1 className="text-3xl font-bold mb-4">Welcome to TomodachiShare!</h1>
+				<h1 className="text-3xl font-bold mb-4">Welcome to the island!</h1>
 
 				<div className="flex items-center gap-4 text-zinc-500 text-sm font-medium mb-6">
 					<hr className="flex-grow border-zinc-300" />
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,10 +12,10 @@ export default function NotFound() {
 		<div className="flex-grow flex items-center justify-center">
 			<div className="bg-amber-50 border-2 border-amber-500 rounded-2xl shadow-lg p-8 max-w-xs w-full text-center flex flex-col">
 				<h2 className="text-7xl font-black">404</h2>
-				<p>Page not found</p>
+				<p>Page not found - you swam off the island!</p>
 				<Link href="/" className="pill button gap-2 mt-8 w-fit self-center">
 					<Icon icon="ic:round-home" fontSize={24} />
-					Return Home
+					Travel Back
 				</Link>
 			</div>
 		</div>
