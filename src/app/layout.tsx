59b0b939c785ff08e714c86ed36afdfd8a6ad1ee
trafficlunar <hello@trafficlunar.net>
2025-09-13 11:53:06 +0100

    fix: suppress hydration warning on json-ld script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,6 +78,7 @@ export default function RootLayout({
 			<head>
 				<script
 					type="application/ld+json"
+					suppressHydrationWarning
 					dangerouslySetInnerHTML={{
 						__html: JSON.stringify(jsonLd).replace(/</g, "\\u003c"),
 					}}
