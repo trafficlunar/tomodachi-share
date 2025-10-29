6ffb85c49cb9776c2d591e4759c3a89b5856d336
trafficlunar <hello@trafficlunar.net>
2025-10-29 19:59:00 +0000

    style: improve descriptions' whitespace

--- src/app/mii/[id]/page.tsx
+++ src/app/mii/[id]/page.tsx
@@ -211,7 +211,11 @@ export default async function MiiPage({ params }: Props) {
 							</div>
 
 							{/* Description */}
-							{mii.description && <p className="text-sm mt-2 ml-2 bg-white/50 p-3 rounded-lg border border-orange-200">{mii.description}</p>}
+							{mii.description && (
+								<p className="text-sm mt-2 ml-2 bg-white/50 p-3 rounded-lg border border-orange-200 whitespace-break-spaces max-h-54 overflow-y-auto">
+									{mii.description}
+								</p>
+							)}
 						</div>
 
 						{/* Buttons */}
