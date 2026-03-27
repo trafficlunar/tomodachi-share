540480b6654970d8671e87de0f2ee134942a922d
trafficlunar <hello@trafficlunar.net>
2026-03-27 12:37:07 +0000

    fix: outdated add mii tutorial for switch

--- src/components/tutorial/switch-add-mii.tsx
+++ src/components/tutorial/switch-add-mii.tsx
@@ -27,15 +27,19 @@ export default function SwitchAddMiiTutorialButton() {
 										imageSrc: "/tutorial/switch/adding-mii/step1.jpg",
 									},
 									{
-										text: "2. Press 'From scratch' and choose the Male template (instructions may be slightly inaccurate if you select Female)",
+										text: "2. Press 'From scratch' and choose the Male template",
 										imageSrc: "/tutorial/switch/adding-mii/step2.jpg",
 									},
 									{
-										text: "3. Follow all instructions (not all instructions will be there, check next slide for more)",
-										imageSrc: "/tutorial/switch/adding-mii/step3.jpg",
+										text: "3. Click on the features image on this page to zoom it in and add all features on the mii editor",
+										imageSrc: "/tutorial/switch/adding-mii/step3.png",
 									},
 									{
-										text: "4. If the instructions have height, distance, etc. the value will be relative to how many times to click the button - positive for up/left, negative for down/right",
+										text: "4. If the author added instructions, follow them (not all instructions will be there, check next slide for more)",
+										imageSrc: "/tutorial/switch/adding-mii/step4.jpg",
+									},
+									{
+										text: "5. For instructions like height or distance, use the number of button clicks (positive for buttons on right, negative for buttons on left)",
 										imageSrc: "/tutorial/switch/step4.jpg",
 									},
 									{ type: "finish" },
