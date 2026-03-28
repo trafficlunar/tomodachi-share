dd651d65627850bbb81ae053bae48026f19a78b0
trafficlunar <hello@trafficlunar.net>
2026-03-28 23:07:26 +0000

    fix: random page breaking?

--- src/app/random/page.tsx
+++ src/app/random/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { prisma } from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export default async function RandomPage() {
 	const count = await prisma.mii.count();
 	if (count === 0) redirect("/");
