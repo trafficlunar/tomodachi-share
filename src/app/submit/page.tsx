147d005a1419188adca1475d16b10f51f16796cc
trafficlunar <hello@trafficlunar.net>
2026-04-01 02:52:09 +0100

    unblock submissions

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -31,16 +31,15 @@ export default async function SubmitPage() {
 	if (activePunishment) redirect("/off-the-island");
 
 	// Check if submissions are disabled
-	// let value: boolean | null = true;
-	// try {
-	// 	const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/admin/can-submit`);
-	// 	value = await response.json();
-	// } catch (error) {
-	// 	return <p>An error occurred!</p>;
-	// }
+	let value: boolean | null = true;
+	try {
+		const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/admin/can-submit`);
+		value = await response.json();
+	} catch (error) {
+		return <p>An error occurred!</p>;
+	}
 
-	// if (!value)
-	return (
+	if (!value) return (
 			<div className="grow flex items-center justify-center">
 				<div className="bg-amber-50 border-2 border-amber-500 rounded-2xl shadow-lg p-8 max-w-xs w-full text-center flex flex-col">
 					<h2 className="text-5xl font-black">Sorry</h2>
@@ -53,5 +52,5 @@ export default async function SubmitPage() {
 			</div>
 	);
 
-	// return <SubmitForm />;
+	return <SubmitForm />;
 }
