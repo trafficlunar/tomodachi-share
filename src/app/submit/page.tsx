8608acae3101b0c8b99765236b992cee6c0bac44
trafficlunar <hello@trafficlunar.net>
2026-04-01 02:14:33 +0100

    temp disable submissions

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -31,22 +31,16 @@ export default async function SubmitPage() {
 	if (activePunishment) redirect("/off-the-island");
 
 	// Check if submissions are disabled
-	let value: boolean | null = true;
-	try {
-		const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/admin/can-submit`);
-		value = await response.json();
-	} catch (error) {
-		return <p>An error occurred!</p>;
-	}
-	try {
-		const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/admin/can-submit`);
-		value = await response.json();
-	} catch (error) {
-		return <p>An error occurred!</p>;
-	}
+	// let value: boolean | null = true;
+	// try {
+	// 	const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/admin/can-submit`);
+	// 	value = await response.json();
+	// } catch (error) {
+	// 	return <p>An error occurred!</p>;
+	// }
 
-	if (!value)
-		return (
+	// if (!value)
+	return (
 			<div className="grow flex items-center justify-center">
 				<div className="bg-amber-50 border-2 border-amber-500 rounded-2xl shadow-lg p-8 max-w-xs w-full text-center flex flex-col">
 					<h2 className="text-5xl font-black">Sorry</h2>
@@ -57,7 +51,7 @@ export default async function SubmitPage() {
 					</Link>
 				</div>
 			</div>
-		);
+	);
 
-	return <SubmitForm />;
+	// return <SubmitForm />;
 }
