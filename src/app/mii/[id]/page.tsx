926dd9dd6b611ef994c161a55d7d0bfd6fca6cfc
trafficlunar <hello@trafficlunar.net>
2026-04-02 20:03:00 +0100

    feat: add duration to queue message

--- src/app/mii/[id]/page.tsx
+++ src/app/mii/[id]/page.tsx
@@ -133,8 +133,8 @@ export default async function MiiPage({ params }: Props) {
 					<div className="bg-zinc-50 border-2 border-zinc-400 rounded-2xl shadow-lg p-4 flex items-center gap-3 text-zinc-700">
 						<Icon icon="material-symbols:timer" className="text-2xl shrink-0" />
 						<p className="font-medium">
-							This Mii is waiting to be manually reviewed {!settings.queueEnabled && "after being auto-flagged for inappropriate images "}
-							and is hidden from the main page.
+							This Mii is waiting to be manually reviewed and is hidden from the main page. (This could take a few hours or a couple of days) You can still
+							share the Mii through the URL!
 						</p>
 					</div>
 				)}
