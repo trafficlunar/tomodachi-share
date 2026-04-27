09a70a87bd309450646a2937127e75b5dd862adf
trafficlunar <hello@trafficlunar.net>
2026-04-27 23:42:00 +0100

    feat: better api index page

--- backend/src/app/page.tsx
+++ backend/src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function IndexPage() {
-	return <p>TomodachiShare API</p>;
+	return (
+		<div className="absolute size-full flex flex-col justify-center items-center">
+			<h1 className="text-2xl font-bold">TomodachiShare API</h1>
+			<p>You probably shouldn't be here</p>
+			<a href="https://tomodachishare.com" className="pill button mt-4">
+				Go back
+			</a>
+		</div>
+	);
 }
